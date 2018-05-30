f7aec5d563cae0de144fa42a99246bec00e7577f
Shawn G. Meyer <shawn.meyer@auth0.com>
2018-05-30 11:16:05 -0500

    Adding a validation method returning boolean value if config can be compiled.

--- test/tests.js
+++ test/tests.js
@@ -13,4 +13,16 @@ describe('trust-proxy', function() {
     const result = validator(undefined, 0);
     assert.ok(result);
   });
+
+  it('should be valid on 127.0.0.1', function() {
+    const data = ['loopback'];
+    const result = trustproxy.validate(data);
+    assert.isTrue(result);
+  });
+
+  it('should be invalid for 0.0.0.0/0', function() {
+    const data = ['0.0.0.0/0'];
+    const result = trustproxy.validate(data);
+    assert.isFalse(result);
+  });
 });
